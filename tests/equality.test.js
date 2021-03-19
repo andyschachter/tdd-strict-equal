a9d3aa0c0a8dab199521f5d575c84864e93471ad
Andy Schachter <andy.schachter@gmail.com>
2021-03-19 16:32:03 -0400

    updated test variables

--- tests/equality.test.js
+++ tests/equality.test.js
@@ -6,23 +6,27 @@ var strictEqual = require('../equality')
 describe('equality', () => {
     describe('strictEqual', () => {
         it('return true if two values are same value and type', () => {
-            var sameSame = (5,5);
-            var testSame = strictEqual(sameSame);
+            var sameSame1 = 5;
+            var sameSame2 = 5;
+            var testSame = strictEqual(sameSame1, sameSame2);
             expect(testSame).to.be.true;
         })
         it('return false if two values are different value but same type', () => {
-            var diffSame = (1,2);
-            var testNumDiff = strictEqual(diffSame);
+            var diffSame1 = 1;
+            var diffSame2 = 2;
+            var testNumDiff = strictEqual(diffSame1, diffSame2);
             expect(testNumDiff).to.be.false;
         })
         it('return false if two values are same value but different types', () => {
-            var sameDiff = (5,'5');
-            var testTypeDiff = strictEqual (sameDiff);
+            var sameDiff1 = 5;
+            var sameDiff2 = '5';
+            var testTypeDiff = strictEqual (sameDiff1, sameDiff2);
             expect(testTypeDiff).to.be.false;
         })
         it('return false if two values are different value and types', () => {
-            var diffDiff = (1,'2');
-            var testTotalDiff = strictEqual (diffDiff);
+            var diffDiff1 = 1;
+            var diffDiff2 = '2';
+            var testTotalDiff = strictEqual (diffDiff1,diffDiff2);
             expect(testTotalDiff).to.be.false;
         })
     })
